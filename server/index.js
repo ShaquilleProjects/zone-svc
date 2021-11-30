de4c91aa9f45dd01a02639165d012ec75cb4727b
Shaquille Shaw <shaquilleshaw@Shaquilles-MBP.hsd1.ca.comcast.net>
2021-11-29 22:26:32 -0800

    packag ejson

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const keys = require('./config/keys.js');
+//const keys = require('./config/keys.js');
 
 app = express();
 
@@ -12,4 +12,9 @@ app.get(`/`, async(req, res) => {
 // docs page
 app.get(`/docs-page`, async(req, res) => {
     res.sendFile(path.join(process.cwd()+'/public/docs-page.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function () {
+    console.log('Express app listening at PORT %s', PORT)
 });
