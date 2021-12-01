89ad739259e69fc1857e86f7fc23598748cab864
Shaquille Shaw <shaquilleshaw@Shaquilles-MBP.hsd1.ca.comcast.net>
2021-11-30 20:25:47 -0800

    sample route

---
+++ server/routes/routesSampleGroup.js
@@ -0,0 +1,28 @@
+
+module.exports= (app) =>{
+
+    const middlewareSample = require('../../middlewares/middlewareSample');
+    const keys = require('../../config/keys.js');
+
+    app.get(`/:id/example`,middlewareSample(req.params.id), async(req, res) => {
+
+        //connect to mongodb
+        const mongoose = require('mongoose');
+        require('../../models/mymodel.js');
+        mongoose.connect(keys.MONGO_URI_MARKUP);
+        const MyModel = mongoose.model('currency');
+
+        //fetch from mongodb and disconnect
+        MyModel.find({pair: currency.pair}, function (err, curr) { //mongodb query
+            res.send(curr[0].monthly);
+            mongoose.disconnect();
+        });
+
+    });
+
+    app.get(`/blah`,middlewareSample(req.params.id), async(req, res) => {
+
+        res.send();
+
+    });
+}
