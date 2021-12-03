0d63451b2be3251e65a5cefd11047de968e3f4eb
Shaquille Shaw <shaquilleshaw@Shaquilles-MBP.hsd1.ca.comcast.net>
2021-12-02 18:04:58 -0800

    fixing the web scraping

--- server/cronjobs/jobs/daily.js
+++ server/cronjobs/jobs/daily.js
@@ -1,46 +1,73 @@
 // WEB SCRAPING EXAMPLE
-const puppeteer = require('puppeteer');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const pretty = require("pretty");
 const mongoose = require('mongoose');
+
 const keys = require('../../config/keys.js');
-
 require('../../models/mymodel.js');
-//we scraping example found here
-// https://github.com/puppeteer/puppeteer/blob/main/examples/cross-browser.js
-
-
-const firefoxOptions = {
-    product: 'firefox',
-    extraPrefsFirefox: {
-        // Enable additional Firefox logging from its protocol implementation
-        // 'remote.log.level': 'Trace',
-    },
-    // Make browser logs visible
-    dumpio: true,
-};
 
 // daily  job
 async function dailyJob(){
-    const browser = await puppeteer.launch(firefoxOptions);
+    const url = "https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3";
+    try {
+        // Fetch HTML of the page we want to scrape
+        const { data } = await axios.get(url);
 
-    const page = await browser.newPage();
-    console.log(await browser.version());
-  
-    await page.goto('https://news.ycombinator.com/');
-  
-    // Extract articles from the page.
-    const resultsSelector = '.storylink';
-    const links = await page.evaluate((resultsSelector) => {
-      const anchors = Array.from(document.querySelectorAll(resultsSelector));
-      return anchors.map((anchor) => {
-        const title = anchor.textContent.trim();
-        return `${title} - ${anchor.href}`;
-      });
-    }, resultsSelector);
-    console.log(links.join('\n'));
-  
-    await browser.close();
-    
-}
+        // Load HTML we fetched in the previous line
+        const $ = cheerio.load(data);
+
+        // Select all the list items in plainlist class
+        const listItems = $(".plainlist ul li");
+
+        // Stores data for all countries
+        const countries = [];
+
+        // Use .each method to loop through the li we selected
+        listItems.each((idx, el) => {
+          // Object holding data for each country/jurisdiction
+          const country = { name: "", iso3: "" };
+          // Select the text content of a and span elements
+          // Store the textcontent in the above object
+          country.name = $(el).children("a").text();
+          country.iso3 = $(el).children("span").text();
+          // Populate countries array with country data
+          countries.push(country);
+        });
+
+        // Logs countries array to the console
+        console.dir(countries);
+
+        // Write countries array in database
+
+      } catch (err) {
+        console.error(err);
+      }
+};
+
+
+
+// CODE TO LOAD JS from a react website
+
+// var options = {
+//     url: user.instagram_url,
+//     headers: {
+//       'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4'
+//     }
+//   };
+
+//   request(options, function(error, response, html) {
+//     if (!error) {
+
+//       console.log('Scraper running on Instagram user page.');
+
+//       // Use Cheerio to load the page.
+//       var $ = cheerio.load(html);
+
+//       // Code to parse the DOM here
+
+//     }
+// }
 
 module.exports = {
     dailyJob
