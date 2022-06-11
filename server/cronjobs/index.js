ff50d0bb3b29dc73d414fb11654a20363f685ceb
Shaquille Shaw <shaquilleshaw@Shaquilles-MacBook-Pro.local>
2022-06-11 16:33:02 -0700

    fixing bug

--- server/cronjobs/index.js
+++ server/cronjobs/index.js
@@ -13,27 +13,27 @@ const {
 async function main(){
 
     //if it is middnight on the 1st and it not a weekedend then find monthly zones followed by weekly zones
-    if( getDayOfWeek()!=='Sunday' && getDayOfWeek()!=='Saturday' && getTimeOfDay()===22 && getDayOfMonth()===1 ){
+    if( getDayOfWeek()!=='Sunday' && getDayOfWeek()!=='Saturday' && getTimeOfDay()===21 && getDayOfMonth()===1 ){
         await monthlyZones();
         await weeklyZones();
     }
     
     //if the first falls on a weekend, find the monthly zones at the following Sunday at 5pm
-    else if( getDayOfWeek()==='Sunday' && (getDayOfMonth()===1 || getDayOfMonth()===2) && getTimeOfDay()===22){
+    else if( getDayOfWeek()==='Sunday' && (getDayOfMonth()===1 || getDayOfMonth()===2) && getTimeOfDay()===21){
 
         await monthlyZones();
         await weeklyZones();
     }
 
     //if it is 5pm on Sunday then find weekly zones
-    else if( getDayOfWeek()==='Sunday' && getTimeOfDay()===22 ){
+    else if( getDayOfWeek()==='Sunday' && getTimeOfDay()===21 ){
 
         await weeklyZones();
 
     }
 
     //if it is 5pm then find daily zones
-    if( getTimeOfDay()===22 ){
+    if( getTimeOfDay()===21 ){
 
         await dailyZones();
 
