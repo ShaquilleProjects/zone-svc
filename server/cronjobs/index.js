3af30193f5728186ec0140702e9405770c176f9e
Shaquille Shaw <shaquilleshaw@Shaquilles-MacBook-Pro.local>
2022-06-13 23:54:53 -0700

    changes

--- server/cronjobs/index.js
+++ server/cronjobs/index.js
@@ -13,31 +13,31 @@ const {
 async function main(){
 
     //if it is middnight on the 1st and it not a weekedend then find monthly zones followed by weekly zones
-    if( getDayOfWeek()!=='Sunday' && getDayOfWeek()!=='Saturday' && getTimeOfDay()===21 && getDayOfMonth()===1 ){
-        await monthlyZones();
-        await weeklyZones();
-    }
+    // if( getDayOfWeek()!=='Sunday' && getDayOfWeek()!=='Saturday' && getTimeOfDay()===21 && getDayOfMonth()===1 ){
+    //     await monthlyZones();
+    //     await weeklyZones();
+    // }
     
     //if the first falls on a weekend, find the monthly zones at the following Sunday at 5pm
-    else if( getDayOfWeek()==='Sunday' && (getDayOfMonth()===1 || getDayOfMonth()===2) && getTimeOfDay()===21){
+    //else if( getDayOfWeek()==='Sunday' && (getDayOfMonth()===1 || getDayOfMonth()===2) && getTimeOfDay()===21){
 
         await monthlyZones();
         await weeklyZones();
-    }
+    // }
 
-    //if it is 5pm on Sunday then find weekly zones
-    else if( getDayOfWeek()==='Sunday' && getTimeOfDay()===21 ){
+    // //if it is 5pm on Sunday then find weekly zones
+    // else if( getDayOfWeek()==='Sunday' && getTimeOfDay()===21 ){
 
-        await weeklyZones();
+    //     await weeklyZones();
 
-    }
+    // }
 
     //if it is 5pm then find daily zones
-    if( getTimeOfDay()===21 ){
+    // if( getTimeOfDay()===21 ){
 
         await dailyZones();
 
-    }
+    // }
 
     // finding hourly zones always runs last becasue 
     // it is the only function that ends with process.exit(0)
